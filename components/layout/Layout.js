0b17f2d9de324505f0657260356841786ecf81d4
peyman malek <p.malek32@gmail.com>
2023-01-25 17:11:53 +0330

    adding data and images

---
+++ components/layout/Layout.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import styled from "./Layout.module.css";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <header className={styled.header}>
+        <Link href="/">
+          <h2>Car Shopping</h2>
+          <p>Choose And Buy Your Car</p>
+        </Link>
+      </header>
+      <div className={styled.container}>{children}</div>
+      <footer className={styled.footer}>
+        &copy;This Site Made By{" "}
+        <a href="https://peyman-malek.netlify.app" target="_blank">
+          Peyman malek
+        </a>
+      </footer>
+    </>
+  );
+};
+
+export default Layout;
